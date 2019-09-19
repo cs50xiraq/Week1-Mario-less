16b2a14249ce8fc8bde98125a5bde8d01b1558bc
Saadmohammeed <saadmohammedsaeed2005@gmail.com>
2019-09-19 17:44:19 +0300

    week1-Assignments

---
+++ mario (1).c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("hegit:");
+    }
+    while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            printf(" ");   
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+                                
